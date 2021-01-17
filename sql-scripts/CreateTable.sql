0069d40aa72d63c1b437b5f89e84ac9bdb2f4012
zmadi <46574765+zmadi1@users.noreply.github.com>
2021-01-17 16:13:59 +0000

    Delete sql-scripts directory

--- sql-scripts/CreateTable.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE zakhele.users (
-id INT AUTO_INCREMENT PRIMARY KEY,
-first_name varchar(25),
-last_name  varchar(25),
-funniest_joke TEXT
-);
--- sql-scripts/InsertData.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO zakhele.users (first_name,last_name,funniest_joke) VALUES ("Zakhele","Madi","What did C++ say to C?. You have no class.");
